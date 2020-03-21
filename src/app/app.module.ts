c372684e15097684abe9c23a8bdb24c86c119ee8
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-21 17:16:21 +0200

    feat(form): trying to add a form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HelloPageComponent } from './hello-page/hello-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { HelloPageComponent } from "./hello-page/hello-page.component";
+import { MatAutocomplete } from "@angular/material/autocomplete";
+import { MatFormField } from "@angular/material/form-field";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HelloPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HelloPageComponent],
+    imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule, MatAutocomplete, MatFormField],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/hello-page/hello-page.component.ts
+++ src/app/hello-page/hello-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 
 @Component({
     selector: "app-hello-page",
@@ -6,7 +9,22 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./hello-page.component.scss"]
 })
 export class HelloPageComponent implements OnInit {
+    myControl = new FormControl();
+    options: string[] = ["One", "Two", "Three"];
+    filteredOptions: Observable<string[]>;
+
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(""),
+            map(value => this._filter(value))
+        );
+    }
+
+    private _filter(value: string): string[] {
+        const filterValue = value.toLowerCase();
+
+        return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    }
 }
