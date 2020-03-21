92ff234a98b7692a44345348ff8c5bb62ee9367a
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-22 00:47:23 +0200

    feat(main-list): added basic grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HelloPageComponent } from "./hello-page/hello-page.component";
-// ANGULAR MATERIAL MODULES
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { FooterComponent } from "./footer/footer.component";
 import { MainListComponent } from "./main-list/main-list.component";
 import { TopBannerComponent } from "./top-banner/top-banner.component";
 import { SearchLocationComponent } from "./search-location/search-location.component";
 import { SearchSmartItemsComponent } from "./search-smart-items/search-smart-items.component";
+
+// ANGULAR MATERIAL MODULES
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
     declarations: [
@@ -34,7 +37,9 @@ import { SearchSmartItemsComponent } from "./search-smart-items/search-smart-ite
         MatFormFieldModule,
         MatInputModule,
         ReactiveFormsModule,
-        FormsModule
+        FormsModule,
+        MatGridListModule,
+        MatTabsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
