e0dbd43e0dc517c5e0795247f720348a4bd1942f
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-21 12:51:45 +0200

    feat(page): hello page generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HelloPageComponent } from './hello-page/hello-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hello-page/hello-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-page',
+  templateUrl: './hello-page.component.html',
+  styleUrls: ['./hello-page.component.sass']
+})
+export class HelloPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
