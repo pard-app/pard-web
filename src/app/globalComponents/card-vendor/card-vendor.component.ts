38d2ed21622bc134a087cc651dee6d8e8c1cb049
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-26 21:27:56 +0200

    feat(listings): styling

--- src/app/globalComponents/card-vendor/card-vendor.component.ts
+++ src/app/globalComponents/card-vendor/card-vendor.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: "app-card-vendor",
-  templateUrl: "./card-vendor.component.html",
-  styleUrls: ["./card-vendor.component.scss"]
+    selector: "app-card-vendor",
+    templateUrl: "./card-vendor.component.html",
+    styleUrls: ["./card-vendor.component.scss"]
 })
 export class CardVendorComponent implements OnInit {
-  @Input() name: string;
-  @Input() description: string;
-  @Input() image: string;
-  @Input() address: string;
-  @Input() id: string;
+    @Input() title: string;
+    @Input() description: string;
+    @Input() image: string;
+    @Input() address: string;
+    @Input() id: string;
 
-  constructor() {}
+    constructor() {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
 }
