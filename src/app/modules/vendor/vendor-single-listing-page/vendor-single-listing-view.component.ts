3ebf86609e76cf9342d2f7b0d2bef679661b6188
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-28 21:00:49 +0200

    feat(vendor): single listing page

--- src/app/modules/vendor/vendor-single-listing-page/vendor-single-listing-view.component.ts
+++ src/app/modules/vendor/vendor-single-listing-page/vendor-single-listing-view.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { DbServiceService } from "@services/db-service.service";
+import { ActivatedRoute } from "@angular/router";
+import { ListingItem } from "@models/listingitem.interface";
+import { Observable, BehaviorSubject, Subscription } from "rxjs";
+import { IVendor } from "@models/vendor.interface";
+import { CartStoreService } from "src/app/@features/stores/cart/cart.store.service";
+import { convertObservableToBehaviorSubject } from "src/app/@features/utils";
 
 @Component({
     selector: "app-vendor-single-listing-view",
@@ -6,7 +13,18 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./vendor-single-listing-view.component.scss"]
 })
 export class VendorSingleListingViewComponent implements OnInit {
-    constructor() {}
+    constructor(private dbService: DbServiceService, private route: ActivatedRoute, private cartStoreService: CartStoreService) {}
+    public listing$: BehaviorSubject<ListingItem>;
+    public vendor$: Observable<IVendor>;
 
-    ngOnInit(): void {}
+    addToCart() {
+        this.cartStoreService.addItemToCart(this.listing$.getValue());
+    }
+
+    ngOnInit(): void {
+        this.route.params.subscribe(({ listingId, vendorId }) => {
+            this.listing$ = convertObservableToBehaviorSubject(this.dbService.getListingById(listingId), null);
+            this.vendor$ = this.dbService.getVendorById(vendorId);
+        });
+    }
 }
