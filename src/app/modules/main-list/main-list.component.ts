1165aac24c480a99db44398c1f4a46a52f85b832
Armands Leimanis <armands@gmail.com>
2020-03-28 12:52:03 +0200

    feat(algolia): added algolia example

--- src/app/modules/main-list/main-list.component.ts
+++ src/app/modules/main-list/main-list.component.ts
@@ -2,37 +2,71 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { DbServiceService } from "src/app/@features/services/db-service.service";
 import { IVendor } from "@models/vendor.interface";
 import { Subscriber, Observable, Subscription } from "rxjs";
-
+import * as algoliasearch from "algoliasearch";
 @Component({
-    selector: "app-main-list",
-    templateUrl: "./main-list.component.html",
-    styleUrls: ["./main-list.component.scss"]
+  selector: "app-main-list",
+  templateUrl: "./main-list.component.html",
+  styleUrls: ["./main-list.component.scss"]
 })
 export class MainListComponent implements OnInit, OnDestroy {
-    private vendorsSubscriber: Subscription;
-    private listingSubscription: Subscription;
-    public vendorsList: Array<IVendor>;
-    public listingsList: Array<any>;
-    public currentCity: string = null;
+  private vendorsSubscriber: Subscription;
+  private listingSubscription: Subscription;
+  public vendorsList: Array<IVendor>;
+  public listingsList: Array<any>;
+  public currentCity: string = null;
+  private searchClient: any;
+  private listingsIndex: any;
+  private vendorsIndex: any;
 
-    constructor(public dataService: DbServiceService) {}
+  constructor(public dataService: DbServiceService) {
+    this.searchClient = algoliasearch(
+      "8A6TCGT3CX",
+      "4ee9375d899179a0701130c2df2c1f76"
+    );
+    this.listingsIndex = this.searchClient.initIndex("listings");
+    this.vendorsIndex = this.searchClient.initIndex("vendors");
+  }
 
-    ngOnInit(): void {
-        this.vendorsSubscriber = this.dataService.getMyListings().subscribe((items: Array<IVendor>) => {
-            this.vendorsList = items;
-        });
-        this.listingSubscription = this.dataService.getListings().subscribe(items => {
-            this.listingsList = items;
-        });
-    }
+  ngOnInit(): void {
+    this.vendorsIndex
+      .search({
+        query: ""
+      })
+      .then(data => {
+        if (data.hits.length) {
+          console.log(data.hits);
+          this.vendorsList = data.hits;
+        }
+      });
 
-    ngOnDestroy(): void {
-        this.vendorsSubscriber.unsubscribe();
-        this.listingSubscription.unsubscribe();
-    }
+    this.listingsIndex
+      .search({
+        query: ""
+      })
+      .then(data => {
+        if (data.hits.length) {
+          console.log(data.hits);
+          this.listingsList = data.hits;
+        }
+      });
 
-    onCityChange(ev) {
-        this.currentCity = ev;
-        this.vendorsSubscriber = this.dataService.getMyListings(this.currentCity).subscribe((items: Array<IVendor>) => (this.vendorsList = items));
-    }
+    // this.vendorsSubscriber = this.dataService.getMyListings().subscribe((items: Array<IVendor>) => {
+    //     this.vendorsList = items;
+    // });
+    // this.listingSubscription = this.dataService.getListings().subscribe(items => {
+    //     this.listingsList = items;
+    // });
+  }
+
+  ngOnDestroy(): void {
+    this.vendorsSubscriber.unsubscribe();
+    this.listingSubscription.unsubscribe();
+  }
+
+  onCityChange(ev) {
+    this.currentCity = ev;
+    this.vendorsSubscriber = this.dataService
+      .getMyListings(this.currentCity)
+      .subscribe((items: Array<IVendor>) => (this.vendorsList = items));
+  }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,3 +60,6 @@ import "zone.js/dist/zone"; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+(window as any).process = {
+  env: { DEBUG: undefined }
+};
