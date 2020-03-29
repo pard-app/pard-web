9f9c291b678bd24544cc008c4d2445101a23a450
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-29 02:27:21 +0200

    feat(component): cart-listing-card

---
+++ src/app/modules/cart/cart-page/cart-listing-card/cart-listing-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-listing-card',
+  templateUrl: './cart-listing-card.component.html',
+  styleUrls: ['./cart-listing-card.component.scss']
+})
+export class CartListingCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
