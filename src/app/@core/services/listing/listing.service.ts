16504a6ba3e36e45d099ab80b616e97babde34b6
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-04-04 23:50:24 +0300

    feat(services): Promises -> Observables

--- src/app/@core/services/listing/listing.service.ts
+++ src/app/@core/services/listing/listing.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { AlgoliaService } from "@services/algolia/algolia.service";
 import { ListingItem } from "src/app/@core/models/listingitem.interface";
-import { from } from "rxjs";
+import { from, Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root",
@@ -10,8 +10,10 @@ import { from } from "rxjs";
 export class ListingService {
     constructor(private algoliaService: AlgoliaService) {}
 
-    public searchListing(query: string = "", hitsPerPage = 50) {
-        return this.algoliaService.listingsIndex.search(query, { hitsPerPage });
+    public searchListing({ query = "", hitsPerPage = 50 } = {}): Observable<ListingItem[] | any> {
+        return from(
+            this.algoliaService.listingsIndex.search<ListingItem[]>(query, { hitsPerPage })
+        );
     }
 
     public searchVendorListings(query = "", vendorId: string, pagination = {}) {
