11b6b88ccedd2d1884dde2c37e7aefce7cee32bb
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-04-15 23:44:47 +0300

    feat(landing): showing closest locations

--- src/app/@modules/main-list/scenarios/nothing/nothing.component.ts
+++ src/app/@modules/main-list/scenarios/nothing/nothing.component.ts
@@ -5,6 +5,7 @@ import { of, Observable } from "rxjs";
 import { map, mergeMap, flatMap, concatMap, mergeAll, concatAll, tap, toArray } from "rxjs/operators";
 import { IVendor } from "@models/vendor.interface";
 import { HttpClient } from "@angular/common/http";
+import { ListingItem } from "@models/listingitem.interface";
 
 @Component({
     selector: "scenario-nothing",
@@ -13,6 +14,8 @@ import { HttpClient } from "@angular/common/http";
 })
 export class NothingComponent implements OnInit, OnDestroy {
     public topVendorsInLocations$: Observable<any> = of([]);
+    public topVendorsNearMe$: Observable<Array<IVendor>>;
+    public topListingsNearMe$: Observable<Array<ListingItem>>;
 
     constructor(private vendorService: VendorService, private listingService: ListingService, private http: HttpClient) {}
 
@@ -22,27 +25,35 @@ export class NothingComponent implements OnInit, OnDestroy {
     }
 
     private async handleTopNearMe() {
-        this.http.get("https://europe-west1-pard-app.cloudfunctions.net/geolocation").subscribe((x) => console.log(x));
+        const { cityLatLong } = (await this.http.get("https://europe-west1-pard-app.cloudfunctions.net/geolocation").toPromise()) as { cityLatLong: string };
+        this.topVendorsNearMe$ = this.vendorService
+            .searchVendor({ query: "", aroundLatLng: cityLatLong, hitsPerPage: 6 })
+            .pipe(mergeMap(async ({ hits }: any) => await this.fillVendorWithItsListings(hits)));
     }
 
     private async handleTopVendorsInLocations() {
-        const vendorsInLocation$ = await this.vendorService.getVendorsInPopularLocations();
-        this.topVendorsInLocations$ = vendorsInLocation$.pipe(
+        const vendorsInLocations$ = await this.vendorService.getVendorsInPopularLocations();
+        this.topVendorsInLocations$ = vendorsInLocations$.pipe(
             flatMap(({ results }) => results),
             mergeMap(async (locationWithVendors: any) => {
                 // Fulfill vendors with their listings
-                const vendors = await Promise.all(
-                    // For every vendor, find his item and place into object
-                    locationWithVendors.hits.map(async (vendor: IVendor) => {
-                        const { hits: listingsOfVendor } = await this.listingService.searchVendorListings("", vendor.objectID, 0, 4);
-                        return { ...vendor, listings: listingsOfVendor };
-                    })
-                );
+                const vendors = await this.fillVendorWithItsListings(locationWithVendors.hits);
                 // Here we're turning vendors[] to observable because main-list-vendors expects an observable
                 return { location: locationWithVendors.query, vendors: of(vendors) };
             }),
             toArray()
         );
     }
+
+    private async fillVendorWithItsListings(vendors: Array<IVendor>): Promise<Array<IVendor>> {
+        // For every vendor, find his item and place into object
+        return Promise.all(
+            vendors.map(async (vendor: IVendor) => {
+                const { hits: listingsOfVendor } = await this.listingService.searchVendorListings("", vendor.objectID, { hitsPerPage: 4 });
+                return { ...vendor, listings: listingsOfVendor } as IVendor;
+            })
+        );
+    }
+
     ngOnDestroy(): void {}
 }
