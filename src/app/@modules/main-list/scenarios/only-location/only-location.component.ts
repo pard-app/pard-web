0cacf83d656c9bd4a98fe39761fc81eaff55cadb
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-04-16 16:11:47 +0300

    feat(init): location-only

--- src/app/@modules/main-list/scenarios/only-location/only-location.component.ts
+++ src/app/@modules/main-list/scenarios/only-location/only-location.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { LocationStore } from "@core/stores/location/location.store";
+import { ListingService } from "@services/listing/listing.service";
+import { debounce } from "rxjs/operators";
+import { interval, Subscription } from "rxjs";
 
 @Component({
     selector: "scenario-only-location",
@@ -6,7 +10,14 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./only-location.component.scss"],
 })
 export class OnlyLocationComponent implements OnInit {
-    constructor() {}
+    private subscriptions = new Subscription();
 
-    ngOnInit(): void {}
+    constructor(public locationStore: LocationStore, private listingService: ListingService) {}
+
+    ngOnInit(): void {
+        const subscribeToGlobalLocationChanges = this.locationStore.currentLocation$.pipe(debounce(() => interval(50))).subscribe(async (currentLocation) => {
+            console.log(currentLocation);
+        });
+        this.subscriptions.add(subscribeToGlobalLocationChanges);
+    }
 }
