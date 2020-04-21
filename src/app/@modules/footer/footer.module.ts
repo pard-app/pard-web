cdb338dab1b875eb2aed025d6c60e4465cdd03d0
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-04-21 23:43:33 +0300

    feat(footer): logic

--- src/app/@modules/footer/footer.module.ts
+++ src/app/@modules/footer/footer.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FooterComponent } from "./footer.component";
+import { NbIconModule, NbButtonModule } from "@nebular/theme";
+import { TranslateModule } from "@ngx-translate/core";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations: [FooterComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, NbIconModule, NbButtonModule, TranslateModule, RouterModule],
     exports: [FooterComponent],
 })
 export class FooterModule {}
