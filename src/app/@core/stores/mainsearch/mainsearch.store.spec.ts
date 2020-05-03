dc22864efdd62d85f6c215497c30aa1adb8799c2
Toma Naujokaitė <juozas.rastenis@gmail.com>
2020-05-04 01:12:03 +0300

    feat(tests): added unit tests for mainsearch inj

---
+++ src/app/@core/stores/mainsearch/mainsearch.store.spec.ts
@@ -0,0 +1,45 @@
+import { TestBed } from "@angular/core/testing";
+
+import { MainSearchStore, SearchRequest } from "./mainsearch.store";
+import { ILocation } from "@models/location.interface";
+
+describe("Global.StoreService", () => {
+    let service: MainSearchStore;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(MainSearchStore);
+    });
+
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
+
+    describe("SearchRequest Manipulation", () => {
+        it("Should start with an empty SearchRequest", (done: DoneFn) => {
+            service.searchRequest$.subscribe((x) => {
+                expect(x).toEqual(new SearchRequest());
+                done();
+            });
+        });
+
+        it("Should construct listingOrVendor correctly", (done: DoneFn) => {
+            service.searchChange({ listingOrVendor: "testValue" });
+            service.searchRequest$.subscribe((x) => {
+                expect(x).toEqual({ listingOrVendor: "testValue", location: null });
+                done();
+            });
+        });
+
+        it("Should construct listingOrVendor correctly", (done: DoneFn) => {
+            service.searchChange({ location: { objectID: "a", _geoloc: { lat: 123, lng: 123 }, name: "a", county: "a", administrative: "a" } });
+            service.searchRequest$.subscribe((x) => {
+                expect(x).toEqual({
+                    listingOrVendor: null,
+                    location: { objectID: "a", _geoloc: { lat: 123, lng: 123 }, name: "a", county: "a", administrative: "a" },
+                });
+                done();
+            });
+        });
+    });
+});
