b0c1ab9d2146484beb0b47751f384280caf5fb67
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-05-13 00:09:32 +0300

    feat(pages): about page module added

---
+++ src/app/@modules/about/about-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AboutComponent } from "./about-page/about.component";
+
+export const ABOUT_ROUTES: Routes = [{ path: "", component: AboutComponent }];
+
+@NgModule({
+    imports: [RouterModule.forChild(ABOUT_ROUTES)],
+    exports: [RouterModule],
+})
+export class VendorRoutingModule {}
