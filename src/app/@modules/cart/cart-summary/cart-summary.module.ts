19c4cef6b62f5bae72521421c88d1386079b7692
vidasdanske <vsti@danskebank.dk>
2020-06-05 21:26:42 +0200

    checkout visual update

--- src/app/@modules/cart/cart-summary/cart-summary.module.ts
+++ src/app/@modules/cart/cart-summary/cart-summary.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { NbCardModule, NbButtonModule } from "@nebular/theme";
+import { NbCardModule, NbButtonModule, NbListComponent, NbListModule } from "@nebular/theme";
 import { TranslateModule } from "@ngx-translate/core";
 import { CartSummaryComponent } from "./cart-summary.component";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations: [CartSummaryComponent],
-    imports: [CommonModule, RouterModule, CommonModule, TranslateModule, NbCardModule, NbButtonModule],
+    imports: [CommonModule, RouterModule, CommonModule, TranslateModule, NbCardModule, NbButtonModule, NbListModule],
     exports: [CartSummaryComponent],
 })
 export class CartSummaryModule {}
